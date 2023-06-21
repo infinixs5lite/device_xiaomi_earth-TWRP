6d83dc4bec987451967e640acb97e92137239fd1
infinixs5lite <kelvinchinedu97@gmail.com>
2023-06-21 22:41:03 +0100

    Update and rename omni_earth.mk to twrp_earth.mk

--- twrp_earth.mk
+++ twrp_earth.mk
@@ -7,16 +7,19 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from earth device
 $(call inherit-product, device/xiaomi/earth/device.mk)
 
 PRODUCT_DEVICE := earth
-PRODUCT_NAME := omni_earth
+PRODUCT_NAME := twrp_earth
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := 22120RN86C
 PRODUCT_MANUFACTURER := xiaomi
