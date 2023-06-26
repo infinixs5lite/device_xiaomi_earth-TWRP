8f6bba4fea1257e57e56bacdb18f616981b5e334
infinixs5lite <kelvinchinedu97@gmail.com>
2023-06-26 18:32:34 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -21,6 +21,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.mt6768
 
+TW_INCLUDE_FASTBOOTD := true
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
