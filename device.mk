dcfb66e621f5eddbafb3b11eaef1380fe11fb807
infinixs5lite <kelvinchinedu97@gmail.com>
2023-06-30 13:50:49 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 # Virtual A/B
 ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Boot Control HAL
 PRODUCT_PACKAGES += \
