180d5b00f20d2822b9b20ce431bfaf36fa2cf507
infinixs5lite <kelvinchinedu97@gmail.com>
2023-07-06 20:34:25 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,10 +16,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
-# API
-PRODUCT_TARGET_VNDK_VERSION := 30
-PRODUCT_SHIPPING_API_LEVEL := 30
-
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
